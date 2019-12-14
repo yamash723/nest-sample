e355c9038204e0a0152bf905e0ebaea491764768
Shuhei Yamashita <b.e7472@gmail.com>
2019-12-14 14:35:28 +0000

    Add GraphQL modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UsersResolver } from './apps/users/users.resolver';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
+  providers: [UsersResolver],
 })
 export class AppModule {}
