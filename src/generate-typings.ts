0f4aa598beed6084b13bb9868317243058c724d0
Shuhei Yamashita <b.e7472@gmail.com>
2019-12-18 06:06:08 +0000

    Add watch mode for generate GraphQL type file

--- src/generate-typings.ts
+++ src/generate-typings.ts
@@ -1,10 +1,12 @@
 import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
 import { join } from 'path';
 
+const isWatch = process.argv[2] === '--watch';
+
 const definitionsFactory = new GraphQLDefinitionsFactory();
 definitionsFactory.generate({
   typePaths: ['./src/**/*.graphql'],
   path: join(process.cwd(), 'src/graphql.ts'),
   outputAs: 'class',
-  watch: true,
+  watch: isWatch,
 });
